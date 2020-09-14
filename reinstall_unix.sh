7693f2408dd99d3ef5ca3fefcc1c67f679d3f93e
Serhii <s_moiseiev@ukr.net>
2020-09-14 15:07:52 +0300

    Update reinstall_unix.sh

--- reinstall_unix.sh
+++ reinstall_unix.sh
@@ -3,10 +3,10 @@
 echo -n "Введите внутрений номер сервера: "
 read server
 
-figlet -f 'Slant Relief' '${server}' -w 300 > /var/www/html/install/bash_banner
+#figlet -f 'Slant Relief' '${server}' -w 300 > /var/www/html/install/bash_banner
 cp /root/mss/script/reinstall_unix_template.sh /var/www/html/install/${server}.sh
 
 echo "************************************************************************"
-echo "Ссылка для скачивания скрипта: http://194.54.88.145/install/${server}.sh"
+echo "Ссылка для скачивания скрипта: http://localhost/install/${server}.sh"
 echo "************************************************************************"
 
