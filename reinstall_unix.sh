d22804cc1657ecd1ba7b767cb3175476a894400e
Serhii <s_moiseiev@ukr.net>
2020-09-14 15:02:41 +0300

    Add files via upload

---
+++ reinstall_unix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -n "Введите внутрений номер сервера: "
+read server
+
+figlet -f 'Slant Relief' '${server}' -w 300 > /var/www/html/install/bash_banner
+cp /root/mss/script/reinstall_unix_template.sh /var/www/html/install/${server}.sh
+
+echo "************************************************************************"
+echo "Ссылка для скачивания скрипта: http://194.54.88.145/install/${server}.sh"
+echo "************************************************************************"
+
