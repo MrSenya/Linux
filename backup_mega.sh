e6a170754645cd9339b27f17b196afbdf2dd66b8
Serhii <s_moiseiev@ukr.net>
2020-09-14 15:03:17 +0300

    Update backup_mega.sh

--- backup_mega.sh
+++ backup_mega.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rclone sync /home/deaxo/Documents/ mega:hosting.ua
-curl -s -X POST https://api.telegram.org/bot1001500810:AAEg1DAZLMvKBXQ8e4YY-lmE-Bn4RXEff8w/sendMessage -d chat_id=-393297331 -d text='Backup done!'
+curl -s -X POST https://api.telegram.org/bot1001500810:AAEg1DAZLMvKgBXQ8e4YY-lmE-Bn4RXEff8w/sendMessage -d chat_id=-3934297331 -d text='Backup done!'
